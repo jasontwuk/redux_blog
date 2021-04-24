e79797eac7f1101c887c5f90cbde387a48cb25bf
Jason <jasontwuk@gmail.com>
2021-04-24 21:40:35 +0100

    add mapStateToProps

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,11 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => {
   return async (dispatch) => {
-    const response = await jsonPlaceholder.get("posts");
+    const response = await jsonPlaceholder.get("/posts");
 
     dispatch({
       type: "FETCH_POSTS",
-      payload: response,
+      payload: response.data,
     });
   };
 };
