75efb97d3eb83273b17cf99ef9959ee921242582
Jason <jasontwuk@gmail.com>
2021-04-27 13:53:52 +0100

    add another action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,26 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+// *** deal with over fetching (users) issue: use another action creator
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // console.log(getState().posts);
+  // *** use _.chain() to chain lowdash functions together
+  // *** use _.map() to pull out all "userId" property from all "posts"
+  // *** use _.uniq() to find the unique ids from all "userId"
+  // *** use .value() to execute all chained functions
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+
+  // *** the following two lines of code are same as the code above
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get("/posts");
@@ -12,27 +32,27 @@ export const fetchPosts = () => {
   };
 };
 
-// export const fetchUser = (id) => {
-//   return async (dispatch) => {
-//     const response = await jsonPlaceholder.get(`/users/${id}`);
+export const fetchUser = (id) => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
 
-//     dispatch({
-//       type: "FETCH_USER",
-//       payload: response.data,
-//     });
-//   };
-// };
-
-// *** deal with over fetching (users) issue: use "lowdash"
-export const fetchUser = (id) => (dispatch) => {
-  _fetchUser(id, dispatch);
+    dispatch({
+      type: "FETCH_USER",
+      payload: response.data,
+    });
+  };
 };
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
+// *** deal with over fetching (users) issue: use "lowdash"
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
 
-  dispatch({
-    type: "FETCH_USER",
-    payload: response.data,
-  });
-});
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: response.data,
+//   });
+// });
